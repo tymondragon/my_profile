a3bb65ba361718d91fd26f1896a3737f697dea4d
Ty Mondragon <tymondragon@gmail.com>
2020-05-05 10:58:26 -0600

    edit site for construction

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -3,23 +3,25 @@ import './Menu.css'
 
 const Menu = ({scrollTo}) => {
     return (
-      <div className="sticky left transparent ">
-        <ul>
-          <li>
+      <div className="sticky left transparent hello">
+        {/* <ul>
+          <li> */}
+        <div className="menu-container">
             <a onClick={(e) => { scrollTo(e) }} id="hello">Hello</a>
-          </li>
-          <li>
+          {/* </li> */}
+          {/* <li>
             <a onClick={(e)=>{scrollTo(e)}} id="whoami">Who Am I?</a></li>
           <li>
             <a onClick={ (e)=> {scrollTo(e) }} id="skills">Skills</a>
           </li>
           <li>
             <a onClick={(e)=>{scrollTo(e)}} id="recentwork">Recent Work</a>
-          </li>
-          <li>
+          </li> */}
+          {/* <li> */}
             <a onClick={(e)=>{scrollTo(e)}} id="stayconnected">Stay Connected</a>
-          </li>
-        </ul>
+          {/* </li>
+        </ul> */}
+        </div>
       </div>
     )
 }
