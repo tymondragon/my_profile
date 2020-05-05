9df322549cb238c9d819c090cbb57d3f21f84ee4
Ty Mondragon <tymondragon@gmail.com>
2020-05-04 18:48:15 -0600

    add email and resume to app

--- src/Hello/Hello.js
+++ src/Hello/Hello.js
@@ -7,7 +7,7 @@ const Hello = () => {
     return (
       <div className="header page">
         <img src={skull} className="App-skull2" alt="logo" />
-          <p>
+          <p className="hello">
             Hello!<br></br>
             I'm Ty Mondragon<br></br>
             {/* scroll to find out more about me<br></br> */}
